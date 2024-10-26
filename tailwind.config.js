f0fbbcce2048aaf00e5b30a005af61957df9011b
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-10-26 12:26:36 +0300

    fix: fixed tailwind.config,js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html', './src/index.css'],
+	safelist: ['mobile', 'desktop'],
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	theme: {
 		extend: {
 			boxShadow: {
